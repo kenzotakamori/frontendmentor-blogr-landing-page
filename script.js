c9401db5219545dd68439ad5cbecc715c2cfcb3f
Marcelo Kenzo Takamori <61280207+kenzotakamori@users.noreply.github.com>
2021-06-02 15:51:16 -0300

    open/close menu

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 let hamburgerIcon = document.getElementsByClassName('hamburger-icon')[0];
 hamburgerIcon.addEventListener('click', () => {
     let hamburgerMenu = document.getElementsByClassName('hamburger-menu')[0];
-    hamburgerMenu.style.display = "block";
+    if (hamburgerMenu.classList.contains('open')) {
+        hamburgerMenu.classList.remove('open');
+    } else {
+        hamburgerMenu.classList.add('open');
+    }
 })
 
 let optionTitle = document.getElementsByClassName('option-title');
